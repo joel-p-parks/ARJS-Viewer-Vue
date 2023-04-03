c388f999b7e826c0bd9297b42d081e6f1964042e
Joel Parks <127913020+joel-p-parks@users.noreply.github.com>
2023-04-03 12:03:33 -0400

    Initial Commit
    Initial project commit.

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <JSViewer v-bind:report="{ Uri: 'report.rdlx-json'}"></JSViewer>
+</div>
+</template>
+
+<script>
+import { Viewer } from '@grapecity/activereports-vue';
+
+export default {
+  name: 'App',
+  components: {
+    JSViewer: Viewer
+  }
+}
+</script>
+
+<style src="../node_modules/@grapecity/activereports/styles/ar-js-ui.css"></style>
+<style src="../node_modules/@grapecity/activereports/styles/ar-js-viewer.css" ></style>
+
+<style>
+#viewer-host {
+  width: 100%;
+  height: 100vh;
+}
+</style>
